fdcb55d5ebfc4572e6f6d7d9247cf098aa641b15
vikputthiscodeongit <code@viktorchin.nl>
2025-10-29 00:38:32 +0100

    build: Update distributables

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,3 @@
-import "./style.css";
 declare enum NotificationState {
     HIDE_BUSY = "HIDE_BUSY",
     SHOW_BUSY = "SHOW_BUSY",
@@ -8,49 +7,45 @@ declare enum NotificationState {
 interface SharedOptions {
     hideAfterTime: number;
     hideOlder: boolean;
-    dismissable: boolean;
+    dismissible: boolean;
 }
 interface NotifierOptions extends SharedOptions {
     parentEl: HTMLElement;
-    position: ["top" | "bottom", "left" | "center" | "right"];
+    position: ["start" | "end", "start" | "center" | "end"];
     classNames: string[];
+    hideButtonElAriaLabelText?: string;
 }
-interface NotificationContent {
-    variant?: string;
+interface NotificationOptions extends Partial<SharedOptions> {
     text?: string | string[];
-    title?: string;
-    titleLevel?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+    title?: string | [string, string];
+    variant?: string;
 }
-interface NotificationOptions extends Partial<SharedOptions>, NotificationContent {
-}
-interface ProcessedNotificationOptions extends SharedOptions, Required<Omit<NotificationContent, "text" | "title">> {
+interface ProcessedNotificationOptions extends Required<Omit<NotificationOptions, "text" | "title">> {
     text: string[] | null;
-    title: string | null;
+    title: [string, string] | null;
 }
-interface NotificationInternal {
+interface Notification extends ProcessedNotificationOptions {
     abortControllers: {
         hideButtonElEvent: AbortController;
         waitForHide: AbortController;
     };
     state: NotificationState;
-}
-interface Notification extends ProcessedNotificationOptions, NotificationInternal {
     el: HTMLDivElement;
 }
 declare class SN {
     #private;
     hideAfterTime: NotifierOptions["hideAfterTime"];
     hideOlder: NotifierOptions["hideOlder"];
-    dismissable: NotifierOptions["dismissable"];
-    notifierEl: HTMLDivElement;
-    notifications: Map<number, Notification>;
-    currentNotificationId: number;
-    queuedNotifications: NotificationOptions[];
-    instanceId: number;
+    dismissible: NotifierOptions["dismissible"];
+    readonly el: HTMLDivElement;
+    readonly notifications: Map<number, Notification>;
+    readonly queue: NotificationOptions[];
+    hideButtonElAriaLabelText: string;
     constructor(options?: Partial<NotifierOptions>);
-    get notificationIds(): number[];
+    get currentId(): number;
+    get ids(): number[];
     show(textOrOptions: NotificationOptions["text"] | NotificationOptions, variant?: NotificationOptions["variant"]): void;
-    hide(notificationId: number): void;
+    hide(id: number): void;
     hideAll(): void;
 }
 export { type NotifierOptions, type NotificationOptions, SN as default };
